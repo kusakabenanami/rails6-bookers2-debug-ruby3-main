1fcee3b260a192bad174e2dc7008d67fae7a2333
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-08-02 13:32:13 +0000

    [Add]2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :is_matching_login_user, only: [:edit,:update]
 
   def show
     @user = User.find(params[:id])
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -30,7 +32,7 @@ class UsersController < ApplicationController
 
   def ensure_correct_user
     @user = User.find(params[:id])
-    unless @user == current_user
+    unless @user == current_user.id
       redirect_to user_path(current_user)
     end
   end
