0ba9d32fef14cc0f48bef987c312a9727a26421c
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-08-09 14:06:46 +0000

    [Add]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,19 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.save
+    @book_comment_new = BookComment.new
     render 'create.js.erb'
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
     render 'destroy.js.erb'
   end
 
