219dd129b4dffb58903ecbb7c77bfff782ab1699
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-08-09 05:50:53 +0000

    [Add]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,9 +2,9 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
     render 'create.js.erb'
   end
 
